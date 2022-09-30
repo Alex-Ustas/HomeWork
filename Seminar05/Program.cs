afbd9500694a5abb597844ade730faafe986dcc0
Alex Ustas <alex.ustas@internet.ru>
2022-09-30 08:27:45 +0300

    add seminar 05 and empty homework 05, forget to add)

--- Seminar05/Program.cs
+++ Seminar05/Program.cs
@@ -1,9 +1,4 @@
-﻿
-string words = Console.ReadLine();
-string[] nums = words.Split(" ");
-
-using System;
-// string words = Console.ReadLine();
+﻿// string words = Console.ReadLine();
 // Console.WriteLine(words);
 // string[] nums = words.Split(",");
 // int index = new Random().Next(0,nums.Length);
@@ -16,20 +11,11 @@ int[] CreatorArray(int size, int min, int max)
     int[] array = new int[size];
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(min,max);
-    }
-    return array;
-}
-int[] NumberArray(int[] array)
-{
-    foreach (var item in array)
-    {
-        item*=-1;
+        array[i] = new Random().Next(min, max);
     }
     return array;
 }
 
-using System;
 // string words = Console.ReadLine();
 // Console.WriteLine(words);
 // string[] nums = words.Split(",");
@@ -38,39 +24,39 @@ using System;
 // Console.WriteLine(String.Join(" ",nums));
 /* Напишите программу замена элементов массива: положительные 
 элементы замените на соответствующие отрицательные, и наоборот.*/
-int[] CreatorArray(int size, int min, int max)
+int[] CreatorArray1(int size, int min, int max)
 {
     int[] array = new int[size];
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(min,max);
+        array[i] = new Random().Next(min, max);
     }
     return array;
 }
-int[] NumberArray(int[] array)
+int[] NumberArray1(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        array[i]*=-1;
+        array[i] *= -1;
     }
     return array;
 }
-int[] array = CreatorArray(8,-10,11);
-Console.WriteLine(String.Join(" ",array));
+int[] array = CreatorArray(8, -10, 11);
+Console.WriteLine(String.Join(" ", array));
 array = NumberArray(array);
-Console.WriteLine(String.Join(" ",array));
+Console.WriteLine(String.Join(" ", array));
 
 bool FindNumber(int number, int[] array)
 {
     foreach (var item in array)
     {
-        if(item == number) 
+        if (item == number)
         {
             return true;
         }
     }
     return false;
 }
-int[] array = CreatorArray(8,-10,11);
-Console.WriteLine(String.Join(" ",array));
-Console.WriteLine(FindNumber(1,array));
+int[] array1 = CreatorArray(8, -10, 11);
+Console.WriteLine(String.Join(" ", array1));
+Console.WriteLine(FindNumber(1, array1));
