eb2dac4a95812a76a1785d887b1c7f8badf56ce6
Alex Ustas <alex.ustas@internet.ru>
2022-10-04 09:31:30 +0300

    add seminar 06 and homework 05

--- Homework05/Program.cs
+++ Homework05/Program.cs
@@ -1,15 +1,162 @@
 ﻿// Homework 05
 
-// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//---------Используемые методы------------
+double[] FillArrayTask1()
+{
 
-// [345, 897, 568, 234] -> 2
+    Random random = new Random();
+    double[] array = new double[random.Next(3, 9)];
 
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
 
-// [3, 7, 23, 12] -> 19
+    return array;
+}
 
-// [-4, -6, 89, 6] -> 0
+double[] FillArrayTask2() //Метод создания и наполнения массива для задачи 2
+{
 
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+    Random random = new Random();
+    double[] array = new double[random.Next(4, 9)];
 
-// [3 7 22 2 78] -> 76
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-100, 101);
+    }
+
+    return array;
+}
+
+double[] FillArrayTask3() //Метод создания и наполнения массива для задачи 3
+{
+
+    Random random = new Random();
+    double[] array = new double[random.Next(4, 9)];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round((random.NextDouble() + random.Next(-100, 100)), 2);
+    }
+
+    return array;
+}
+
+void PrintArray(double[] array) //Метод печати массива
+{
+    Console.WriteLine("[" + String.Join("; ", array) + "]");
+    // Console.Write("[ "); ---- Альтернатива длинная
+    // for (int i = 0; i < array.Length; i++)
+    //     if (i < array.Length - 1)
+    //         Console.Write(array[i] + "; ");
+    //     else
+    //         Console.Write(array[i] + " ");
+    // Console.Write("]");
+}
+
+void NumChet(double[] array) // Метод отображения количества чётных чисел в массиве
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    Console.WriteLine("Количество четных чисел в массиве: " + count);
+}
+
+void SumNechet(double[] array) //Метод отображения суммы элементов, стоящих на нечётных позициях
+{
+    double sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0) sum = sum + array[i];
+    }
+    Console.WriteLine("Сумма элементов, стоящих на нечётных позициях равна: " + sum);
+}
+
+void Differense(double[] array)
+{
+    double Max = array[0];
+    double Min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > Max)
+        {
+            Max = array[i];
+        }
+        else if (array[i] < Min)
+        {
+            Min = array[i];
+        }
+    }
+    Console.WriteLine("Разница между максимальным и минимальным элементами массива равна " + Math.Round((Max - Min), 2));
+}
+
+void task1()
+{
+    // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+    // [345, 897, 568, 234] -> 2
+    Console.Clear();
+    Console.WriteLine("Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
+    double[] array = FillArrayTask1();
+    PrintArray(array);
+    Console.WriteLine();
+    NumChet(array);
+    Console.ReadKey();
+}
+
+void task2()
+{
+    // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+    // [3, 7, 23, 12] -> 19
+    // [-4, -6, 89, 6] -> 0
+    Console.Clear();
+    Console.WriteLine("Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
+    double[] array = FillArrayTask2();
+    PrintArray(array);
+    Console.WriteLine();
+    SumNechet(array);
+    Console.ReadKey();
+}
+
+void task3()
+{
+    // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+    // [3 7 22 2 78] -> 76
+    Console.Clear();
+    Console.WriteLine("Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.");
+    double[] array = FillArrayTask3();
+    PrintArray(array);
+    Differense(array);
+    Console.ReadKey();
+}
+
+
+
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Выберите задачу, которую хотите решить:\n\t1 - Задача 34 \n\t2 - Задача 36 \n\t3 - Задача 38 \n\t4 - Выход");
+    int num = Convert.ToInt32(Console.ReadLine());
+    switch (num)
+    {
+        case 1:
+            task1();
+            Main();
+            break;
+        case 2:
+            task2();
+            Main();
+            break;
+        case 3:
+            task3();
+            Main();
+            break;
+        case 4:
+            Console.WriteLine("Нажмите Enter для выхода");
+            Console.ReadKey();
+            break;
+    }
+}
+Main();
---
+++ Seminar06/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+// string words = Console.ReadLine();
+// Console.WriteLine(words);
+// string[] nums = words.Split(",");
+// int index = new Random().Next(0,nums.Length);
+// Console.WriteLine(nums[index]);
+// Console.WriteLine(String.Join(" ",nums));
+/* Напишите программу замена элементов массива: положительные 
+// элементы замените на соответствующие отрицательные, и наоборот.*/
+// int[] CreatorArray(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(min,max);
+//     }
+//     return array;
+// }
+// int[] NumberArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i]*=-1;
+//     }
+//     return array;
+// }
+// bool FindNumber(int number, int[] array)
+// {
+//     // foreach (var item in array)
+//     // {
+//     //     if(item == number) 
+//     //     {
+//     //         return true;
+//     //     }
+//     // }
+//     // return false;
+//     return array.Contains(number);
+// }
+// int[] DO(int[] array, int min, int max)
+// {
+//     int[] coint = new int[array.Length];
+//     int index = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] > min && array[i] < max){
+//             coint[index] = array[i];
+//             index++;
+//         }
+//     }
+//     return coint;
+// }
+// int[] array = CreatorArray(8,-10,11);
+// Console.WriteLine(String.Join(" ",array));
+// Console.WriteLine(String.Join(" ",DO(array,-2,6)));
+// //Console.WriteLine(FindNumber(1,array));
+
+// void WriteArrayMatrix(string[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+// void WriteArrayMatrix(string[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// WriteArrayMatrix(matrix);
+
+// void Game(string[,] matrix, int X, int Y)
+// {
+//     while (true)
+//     {
+//         matrix[Y,X] = " ";
+//         ConsoleKeyInfo user_keyTab = Console.ReadKey();
+//         if(user_keyTab.Key == ConsoleKey.W) Y--;
+//         if(user_keyTab.Key == ConsoleKey.S) Y++;
+//         if(user_keyTab.Key == ConsoleKey.A) X--;
+//         if(user_keyTab.Key == ConsoleKey.D) X++;
+//         matrix[Y,X] = "@";
+//         Console.Clear();
+//         WriteArrayMatrix(matrix);
+//     }  
+// }
+
